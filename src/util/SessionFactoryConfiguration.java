23860cef2025b3d47779e956cb7eea9e3fcf0f2e
Punsara Kasthurisinghe <kasthurisinghekdpk@gmail.com>
2023-09-07 09:26:05 +0530

    session created

---
+++ src/util/SessionFactoryConfiguration.java
@@ -0,0 +1,25 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfiguration {
+    // singleton;
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private SessionFactoryConfiguration() {
+        Configuration configuration = new Configuration().configure();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance() {
+        return sessionFactoryConfiguration == null ? sessionFactoryConfiguration = new SessionFactoryConfiguration()
+                : sessionFactoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
