2c03df8e272f1716200be01aec7a4613167330ba
Punsara Kasthurisinghe <kasthurisinghekdpk@gmail.com>
2023-09-07 18:31:22 +0530

    hql select all from customer

--- src/App.java
+++ src/App.java
@@ -1,12 +1,19 @@
 
 
+import java.util.List;
+
+import org.hibernate.query.Query;
+
 import entity.CustomerEntity;
 import repository.CustomerRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
         CustomerRepository customerRepository = new CustomerRepository();
-        CustomerEntity customerEntity=customerRepository.getCustomer("C005");
-        System.out.println(customerEntity.toString());
+        List<CustomerEntity> custEntities=customerRepository.getAllCustomers();
+        custEntities.forEach(e->{
+            System.out.println(e.toString());
+        });
     }
+    
 }
--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -1,15 +1,25 @@
 package repository;
 
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import entity.CustomerEntity;
 import util.SessionFactoryConfiguration;
 
 public class CustomerRepository {
-    Session session=SessionFactoryConfiguration.getInstance().getSession();
+    Session session = SessionFactoryConfiguration.getInstance().getSession();
 
-    public CustomerEntity getCustomer(String id){
-        CustomerEntity customerEntity=session.get(CustomerEntity.class,id);
+    public CustomerEntity getCustomer(String id) {
+        CustomerEntity customerEntity = session.get(CustomerEntity.class, id);
         return customerEntity;
+    }
+
+    public List<CustomerEntity> getAllCustomers() {
+        String hql = "FROM CustomerEntity";
+        Query query = session.createQuery(hql);
+        List<CustomerEntity> customerEntities = query.list();
+        return customerEntities;
     }
 }
