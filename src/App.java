a86b04ccd1eab882d3a6833a01ded9a38d2b62db
Punsara Kasthurisinghe <kasthurisinghekdpk@gmail.com>
2023-09-08 12:19:24 +0530

    select where and name parameters

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 
-
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.query.Query;
@@ -9,11 +9,38 @@ import repository.CustomerRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         CustomerRepository customerRepository = new CustomerRepository();
-        List<CustomerEntity> custEntities=customerRepository.getAllCustomers();
-        custEntities.forEach(e->{
-            System.out.println(e.toString());
-        });
+        ///////////////////////////////////////////////////////////////////////////
+        /*
+         * List<CustomerEntity> custEntities=customerRepository.getAllCustomers();
+         * custEntities.forEach(e->{
+         * System.out.println(e.toString());
+         * });
+         */
+        ///////////////////////////////////////////////////////////////////////////
+        /*
+         * List<Object[]> data = customerRepository.getAllCustomerNativeQuery();
+         * for (Object[] row : data) {
+         * System.out.println(Arrays.toString(row));
+         * }
+         */
+        ///////////////////////////////////////////////////////////////////////////
+        /*
+         * List<Object[]> data = customerRepository.getAllCustomerNameAndDob();
+         * for (Object[] row : data) {
+         * System.out.println(Arrays.toString(row));
+         * }
+         */
+        ///////////////////////////////////////////////////////////////////////////
+        /*
+         * CustomerEntity customerEntity=customerRepository.findCustomerById("C005");
+         * System.out.println(customerEntity.toString());
+         */
+
+        ///////////////////////////////////////////////////////////////////////////
+        CustomerEntity customerEntity = customerRepository.findCustomerByIdUsingNameParameters("C005");
+        System.out.println(customerEntity.toString());
     }
-    
+
 }
--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -16,10 +16,44 @@ public class CustomerRepository {
         return customerEntity;
     }
 
+    // hibernate queries;
     public List<CustomerEntity> getAllCustomers() {
         String hql = "FROM CustomerEntity";
         Query query = session.createQuery(hql);
         List<CustomerEntity> customerEntities = query.list();
         return customerEntities;
+    }
+
+    // as the follows we can use native mysql queries in hibernate;
+    public List<Object[]> getAllCustomerNativeQuery() {
+        String sql = "select* from Customer";
+        Query query = session.createSQLQuery(sql);
+        List<Object[]> data = query.list();
+        return data;
+    }
+
+    public List<Object[]> getAllCustomerNameAndDob() {
+        String hql = "select c.id,c.name,c.dob from CustomerEntity as c";
+        // the above c.id,c.name,c.dob ar according to the column names in the customerEntity class
+        // the reason for the table name "CustomerEntity" is here we named the class name as customerEntity
+        Query query = session.createQuery(hql);
+        List<Object[]> data = query.list();
+        return data;
+    }
+    // to get a customer details after passing the id of a customer;
+    public CustomerEntity findCustomerById(String id){
+        String hql = "FROM CustomerEntity WHERE id ='"+id+"'";
+        Query query=session.createQuery(hql);
+        CustomerEntity customerEntity = (CustomerEntity) query.uniqueResult();
+        return customerEntity;
+    }
+
+    public CustomerEntity findCustomerByIdUsingNameParameters(String id){
+        String hql="From CustomerEntity Where id=:id";
+        Query query=session.createQuery(hql);
+        query.setParameter("id",id);
+
+        CustomerEntity customerEntity = (CustomerEntity) query.uniqueResult();
+        return customerEntity;
     }
 }
